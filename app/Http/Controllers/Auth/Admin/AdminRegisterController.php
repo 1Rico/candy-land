8cd65ef3a3af73a3d9ba3815d9a3797181b3e446
Chukwuemeka Okoroafor <emeka.devsq@olotusquare.co>
2018-08-05 16:46:22 +0100

    Created Admin Module Added admin orders, users and tailors viewa Admin Login Setup Modified the Measurement modules to check for null values Added a measurement creation view if user making an order and has not added a measuremtn General Bug DFixes Modified the redirectifAuthenticated middleware to direct auth to intended routes Gave Tailor ability to modify profile Orders are visible to tailors, users and admin and status can be set on orders

---
+++ app/Http/Controllers/Auth/Admin/AdminRegisterController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Tailor;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Tailor;
+use Auth;
+
+class TailorRegisterController extends Controller
+{
+    protected $redirectPath = '/tailor';
+
+
+    protected function Register(Request $request)
+    {
+
+        $rule = [
+            'firstname' => 'required|string|max:255',
+            'lastname' => 'required|string|max:255',
+            'phone' => 'required|string|max:15|unique:tailors',
+            'email' => 'required|string|email|max:255|unique:tailors',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+
+        $this->validate($request, $rule);
+        $tailor = new Tailor();
+        $password = $request['password'];
+        $tailor = $tailor->create([
+            'firstname' => $request['firstname'],
+            'lastname' => $request['lastname'],
+            'gender' => $request['gender'],
+            'phone' => $request['phone'],
+            'email' => $request['email'],
+            'password' => bcrypt($password)
+//            'avatar' => 'public/defaults/avatars/default.png'
+        ]);
+
+        if($tailor && Auth::guard('tailor')->attempt(['email' => $tailor->email, 'password' => $password])){
+//            dd(Auth::guard('tailor')->User()->status);
+
+            // if successful, then redirect to their intended location
+            return redirect()->intended(route('tailor.dashboard'));
+        }
+
+        return redirect()->back()->with('failure', 'An error occurred. Please try again');
+
+    }
+}
